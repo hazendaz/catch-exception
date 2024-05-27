53ad18dc94a831a116a2bfa2c7923285e724d1d0
Jeremy Landis <jeremylandis@hotmail.com>
2024-05-27 18:14:49 -0400

    [packaging] Try to add more packaging to fix javadoc issue

--- catch-throwable/src/main/java/com/googlecode/catchexception/throwable/matcher/package-info.java
+++ catch-throwable/src/main/java/com/googlecode/catchexception/throwable/matcher/package-info.java
@@ -13,4 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.googlecode.catchexception.throwable.test.apis;
+/**
+ * Test matcher package.
+ */
+package com.googlecode.catchexception.throwable.matcher;
